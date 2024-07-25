0ef366b428cce3a44c99f2e47f8b8fce4ec676db
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-07-25 14:31:29 +0700

    Create testOperate.java

---
+++ testOperate.java
@@ -0,0 +1,15 @@
+public class testOperate {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5+2*4;
+        y = (5+2)*4;
+        float value;
+        value = 1;
+        System.out.println("x = " +x);
+        System.out.println("y = " +y);
+        x = y / 3;
+        System.out.println("x = " +x);
+        value = 4 / 3;
+        System.out.println("value = " +value);
+    }
+}
