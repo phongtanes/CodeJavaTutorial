4a51d8c315f6d5586bde8c7538aaf78285bfa20c
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-07-25 14:31:23 +0700

    Create testNum.java

---
+++ testNum.java
@@ -0,0 +1,10 @@
+public class testNum {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = x+1;
+        System.out.println("x = " + x + " y = " + y);
+        y = y++;
+        System.out.println("x = " + x + " y = " + y);
+    }
+}
