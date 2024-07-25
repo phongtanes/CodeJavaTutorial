5c389002ff543b915fb5520249644c283f5f00a7
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-07-25 14:31:37 +0700

    Create stringDemo.java

---
+++ stringDemo.java
@@ -0,0 +1,9 @@
+public class stringDemo {
+    public static void main(String[] args) {
+        String s1;
+        s1 = "Good morning";
+        String name = "Computer";
+        System.out.println(s1 + "" +name);
+    }
+    
+}
