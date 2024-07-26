84217f31e58b59a86f1ae43e91407621ab9c43e1
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-07-26 14:52:39 +0700

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String name = "Computer";
+        int stringSize;
+        stringSize = name.length();
+        System.out.println(name+" has " +stringSize+ " characters");
+
+    }
+    
+}
