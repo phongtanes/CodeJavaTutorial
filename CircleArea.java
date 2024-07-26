8f273671dbc09045a39aafc017dd92f1fe26ecd0
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-07-26 14:52:49 +0700

    Create CircleArea.java

---
+++ CircleArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the radius : ");
+        radius = in.nextDouble();
+        if(radius < 0)
+            System.out.println("Error: Radius must be >= 0");
+        else{
+            double area = Math.PI * radius * radius;
+            System.out.println("Area of the circle is " + area);
+        }
+}
+}
