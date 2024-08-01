255c2c67815309b6facf078d31707157a9b0320e
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-08-01 14:34:49 +0700

    Create testdowhile.java

---
+++ testdowhile.java
@@ -0,0 +1,20 @@
+public class testdowhile {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        int iwhile = 1;
+        while(iwhile <= 10){
+            System.out.println(iwhile);
+            iwhile++;
+        }
+
+        int counter = 1;
+        do {
+            System.out.println(counter);
+            counter++;
+            } while (counter <= 10);
+            
+}
+}
