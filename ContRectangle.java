dd5aac5f3efe5bf5726bd3a4d04b081d6c00aa47
phongtanes <152240597+phongtanes@users.noreply.github.com>
2024-08-09 12:07:59 +0700

    Create ContRectangle.java

---
+++ ContRectangle.java
@@ -0,0 +1,22 @@
+public class ContRectangle {
+    private double length;
+    private double width;
+
+    public ContRectangle(){
+        length = 1.0;
+        width = 1.0;
+    }
+    public ContRectangle(double len, double w) {
+        length = len;
+        width = w;
+    }
+    public double getLenght(){
+        return length;
+    }
+    public double getWidth(){
+        return width;
+    }
+    public double getArea(){
+        return length * width;
+    }
+}
